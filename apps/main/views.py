6faafe7d728cf7edad10e6314afbb94bdf1a042a
wjidjiankang <shenjiankang@outlook.com>
2023-06-09 23:40:08 +0800

    20230609

--- apps/main/views.py
+++ apps/main/views.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from flask import render_template, session, redirect, url_for, abort, request, current_app
+from flask import render_template, session, redirect, url_for, abort, request, current_app,flash
 from . import main
 from .forms import NameForm,PostForm
 from .. import db
@@ -64,4 +64,22 @@ def for_moderators_only():
 def post(id):
     post = Post.query.get_or_404(id)
     return render_template('post.html', posts=[post])
+
+
+@main.route('/edit/<int:id>', methods=['GET', 'POST'])
+@login_required
+def edit(id):
+    post = Post.query.get_or_404(id)
+    if current_user != post.author and \
+                not current_user.can(Permission.ADMIN):
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.body = form.body.data
+        db.session.add(post)
+        db.session.commit()
+        flash('The post has been updated.')
+        return redirect(url_for('.post', id=post.id))
+    form.body.data = post.body
+    return render_template('edit_post.html', form=form)
 
